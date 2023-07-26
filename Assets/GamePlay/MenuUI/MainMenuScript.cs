3a6f89ff69f4d2ecd703f452ab7740b16a2e6174
Bland_nothing <2067721994@qq.com>
2023-07-26 09:17:26 +0800

    主菜单，卷轴展开效果

---
+++ Assets/GamePlay/MenuUI/MainMenuScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class MainMenuScript : MonoBehaviour
+{
+    [SerializeField]
+    GameObject settingsPanel;
+    [SerializeField]
+    GameObject campaignPanel;
+    public void ShowSettingsPanel(){
+        settingsPanel.SetActive(true);
+        ShowScrolls(settingsPanel.transform.Find("MiddleInformation").GetComponent<RectTransform>());
+    }
+     public void ShowCampaignPanel(){
+        campaignPanel.SetActive(true);
+        ShowScrolls(campaignPanel.transform.Find("MiddleInformation").GetComponent<RectTransform>());
+    }
+
+    public void ShowScrolls(RectTransform rectTransform){
+        rectTransform.sizeDelta = new Vector2(0, 1008);
+        rectTransform.DOSizeDelta(new Vector2(1704, 1008),1f);
+    }
+}
