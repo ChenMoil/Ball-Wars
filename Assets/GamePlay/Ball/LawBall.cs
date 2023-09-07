4b73689991afe176ceebfa4c16d571728edc68b1
ChenMoil <2308049967@qq.com>
2023-09-07 12:33:06 +0800

    法师完成部分

---
+++ Assets/GamePlay/Ball/LawBall.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class LawBall: MonoBehaviour  //法球
+{
+    [NonSerialized] public int Damage;  //武器攻击力
+    public BallBlackBoard.Faction lawBallFation;
+    private GameObject Explode;          //爆炸效果
+    private void Start()
+    {
+        Explode = transform.Find("Explode").gameObject;
+        StartCoroutine(DestoryLawBall(5f));
+    }
+    private void OnCollisionEnter2D(Collision2D collision)  //法球与其他物体发生碰撞
+    {
+        if (collision.gameObject.tag == "BallSoldier" && lawBallFation != collision.gameObject.GetComponent<BallAi>().ballBlackBoard.ballFaction)
+        {
+            Explode.GetComponent<LawBallExplode>().StartCon();
+            StopAllCoroutines();
+            StartCoroutine(DestoryLawBall(1f));
+            gameObject.GetComponent<SpriteRenderer>().enabled = false;
+            gameObject.GetComponent<BoxCollider2D>().enabled = false;
+            //collision.gameObject.GetComponent<BallAi>().DeductHP(Damage);  //扣血
+            //Destroy(gameObject);
+        }
+        else if(collision.gameObject.tag == "Boundary")
+        {
+            Destroy(gameObject);
+        }
+    }
+    IEnumerator DestoryLawBall(float time) //定时销毁法球
+    {
+        yield return new WaitForSeconds(time);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/GamePlay/Ball/LawBallExplode.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class LawBallExplode : MonoBehaviour
+{
+    public float maxSize = 4f;
+    private float explodeTime = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //StartCoroutine(ResizeLawBall());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+
+    public void StartCon()
+    {
+        StartCoroutine(ResizeLawBall());
+    }
+    IEnumerator ResizeLawBall() //改变法球的大小
+    {
+        transform.DOScale(maxSize, explodeTime / 2);
+        yield return new WaitForSeconds(explodeTime / 2);
+        transform.DOScale(0, explodeTime / 2);
+    }
+}
